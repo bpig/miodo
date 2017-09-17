c736d2813b1e1e06bfbf6c5ee9842289bb1fd7af
shuai.li <286287737@qq.com>
2017-09-17 17:09:14 +0800

    deep layers

--- ctx/nn_model.py
+++ ctx/nn_model.py
@@ -30,15 +30,23 @@ def read():
 def train():
     kv = read()
     sparse_dim = 27088502
+    layers = [128, 128, 128]
     fea = tf.sparse_merge(kv['fid'], kv['fval'], sparse_dim)
 
-    weights = tf.get_variable("weights", [sparse_dim, 128])
-    biases = tf.get_variable("biases", [128], initializer=tf.zeros_initializer)
+    weights = tf.get_variable("weights", [sparse_dim, layers[0]],
+                              initializer=tf.glorot_uniform_initializer())
+    biases = tf.get_variable("biases", [layers[0]], initializer=tf.zeros_initializer)
 
     embed = tf.nn.embedding_lookup_sparse(
         weights, fea, None, combiner="sum") + biases
 
-    logits = tf.layers.dense(embed, 1, name="logit")
+    l1 = tf.layers.dense(embed, layers[1], name="l1", activation=tf.nn.relu,
+                         kernel_initializer=tf.glorot_uniform_initializer())
+    l2 = tf.layers.dense(l1, layers[2], name="l2", activation=tf.nn.relu,
+                         kernel_initializer=tf.glorot_uniform_initializer())
+    logits = tf.layers.dense(l2, 1, name="logists",
+                             kernel_initializer=tf.glorot_uniform_initializer())
+
     xe = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=tf.to_float(kv['label']))
     loss = tf.reduce_mean(xe)
 
