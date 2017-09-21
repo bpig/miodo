5683b0203b3e2f7593126d016d2f7b5575b61f97
Lu zilong <luzilong@xiaomi.com>
2017-09-21 14:09:43 +0800

    update....

--- ctx/dump_hdfs.py
+++ ctx/dump_hdfs.py
@@ -5,10 +5,15 @@ from common import *
 
 
 def dump():
-    dirname = "/user/h_miui_ad/dev/wwxu/exp/tf/rfrecord/base/date=%2d"
-    date = range(21, 34)
+    # dirname = "/user/h_miui_ad/dev/wwxu/exp/tf/rfrecord/base/date=%2d"
+    dirname = "/user/h_miui_ad/dev/wwxu/exp/tf/rfrecord/opt5_1/date=%2d"
+    date = range(20, 31)
     prefix = "hadoop --cluster c3prc-hadoop fs -get "
-    os.chdir("data")
+    top_dir = dirname.split("/")[-2]
+    print top_dir
+    if not os.path.exists(top_dir):
+        os.mkdir(top_dir)
+    os.chdir(top_dir)
     for d in date:
         cmd = prefix + dirname % d
         print time.ctime(), cmd
