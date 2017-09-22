0e09276b6c3fcb901f110c20dac03b2c4099e7c8
shuai.li <286287737@qq.com>
2017-09-22 21:59:28 +0800

    deep layers

--- ctx/afm.py
+++ ctx/afm.py
@@ -13,7 +13,7 @@ class AFM(NET):
         'iid': tf.FixedLenFeature(1, tf.int64),
     }
 
-    def inference(self, fea, keep_prob=0.5):
+    def inference(self, fea, drop=0.5):
         fea = fea['fid']
         self.att_dim = self.hidden_factor
         self._initialize_weights()
@@ -34,6 +34,7 @@ class AFM(NET):
             tf.square(embed), idx, segment_ids)
 
         fm = 0.5 * tf.subtract(square_last_embed, mean_last_embed)
+        fm = tf.layers.dropout(fm, drop)
         # attention model
 
         att_state = tf.add(tf.matmul(fm, weights['att_W']), weights['att_bias'])
@@ -48,7 +49,7 @@ class AFM(NET):
         for i in range(0, len(self.layer_dim)):
             fm = tf.add(tf.matmul(fm, weights['l%d' % i]), weights['b%d' % i])
             fm = tf.nn.relu(fm)
-            fm = tf.nn.dropout(fm, keep_prob)
+            fm = tf.layers.dropout(fm, drop)
 
         fm = tf.matmul(fm, weights['pred']) + weights['bias']
         emb_bias = tf.nn.embedding_lookup_sparse(weights['emb_bias'], fea, None, combiner="mean")
@@ -56,34 +57,39 @@ class AFM(NET):
 
     def _initialize_weights(self):
         all_weights = dict()
-        glorot = tf.uniform_unit_scaling_initializer()
+        init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.sparse_dim)))
         all_weights['emb_weight'] = tf.get_variable(
-            shape=[self.sparse_dim, self.hidden_factor], initializer=glorot, name='feature_embeddings')
+            shape=[self.sparse_dim, self.hidden_factor], initializer=init, name='feature_embeddings')
 
         all_weights['emb_bias'] = tf.get_variable(
             shape=[self.sparse_dim, 1], initializer=tf.zeros_initializer, name='feature_bias')
 
+        init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.att_dim)))
         all_weights['att_h'] = tf.get_variable(
-            shape=[self.att_dim, 1], initializer=glorot, name='att_h')
+            shape=[self.att_dim, 1], initializer=init, name='att_h')
+        init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.hidden_factor)))
         all_weights['att_W'] = tf.get_variable(
-            shape=[self.hidden_factor, self.att_dim], initializer=glorot, name='att_W')
+            shape=[self.hidden_factor, self.att_dim], initializer=init, name='att_W')
         all_weights['att_bias'] = tf.get_variable(
-            shape=[self.att_dim], initializer=glorot, name='att_bias')
+            shape=[self.att_dim], initializer=tf.zeros_initializer, name='att_bias')
 
         assert self.layer_dim
+        init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.hidden_factor)))
         all_weights['l0'] = tf.get_variable(
-            name="l0", initializer=glorot, shape=[self.hidden_factor, self.layer_dim[0]], dtype=tf.float32)
+            name="l0", initializer=init, shape=[self.hidden_factor, self.layer_dim[0]], dtype=tf.float32)
         all_weights['b0'] = tf.get_variable(
             name="b0", initializer=tf.zeros_initializer, shape=[self.layer_dim[0]], dtype=tf.float32)
 
         for i in range(1, len(self.layer_dim)):
+            init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.layer_dim[i - 1])))
             all_weights['l%d' % i] = tf.get_variable(
-                name="l%d" % i, initializer=glorot, shape=self.layer_dim[i - 1:i + 1], dtype=tf.float32)
+                name="l%d" % i, initializer=init, shape=self.layer_dim[i - 1:i + 1], dtype=tf.float32)
             all_weights['b%d' % i] = tf.get_variable(
                 name="b%d" % i, initializer=tf.zeros_initializer, shape=[self.layer_dim[i]], dtype=tf.float32)
 
+        init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.layer_dim[- 1])))
         all_weights['pred'] = tf.get_variable(
-            name="pred", initializer=glorot, shape=[self.layer_dim[-1], 1], dtype=tf.float32)
+            name="pred", initializer=init, shape=[self.layer_dim[-1], 1], dtype=tf.float32)
 
         all_weights['bias'] = tf.Variable(0.0, name='bias')
         self.weights = all_weights
