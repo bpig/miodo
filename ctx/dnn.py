92b126d7e3d288dd365453e705557084d28741f1
odo.wwxu <email.wwxu>
2017-09-23 10:30:23 +0800

    update....

--- ctx/dnn.py
+++ ctx/dnn.py
@@ -18,21 +18,21 @@ class DNN(NET):
                                    training=self.training, momentum=0.9)
 
         with tf.variable_scope("embed"):
-            # init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.sparse_dim)))
-            init = tf.zeros_initializer
+            init = tf.truncated_normal_initializer(stddev=1.0 / math.sqrt(float(self.sparse_dim)))
+            # init = tf.zeros_initializer
             weights = tf.get_variable("weights", [self.sparse_dim, self.layer_dim[0]],
                                       initializer=init)
             biases = tf.get_variable("biases", [self.layer_dim[0]], initializer=tf.zeros_initializer)
             tf.summary.histogram("weights", weights)
             tf.summary.histogram("biases", biases)
-            embed = tf.nn.embedding_lookup_sparse(weights, fea, None, combiner="mean") + biases
+            embed = tf.nn.embedding_lookup_sparse(weights, fea, None, combiner="sum") + biases
 
         with tf.variable_scope("deep"):
             pre_layer = embed
             for i in range(1, len(self.layer_dim)):
-                # init = tf.truncated_normal_initializer(
-                #     stddev=1.0 / math.sqrt(float(self.layer_dim[i - 1])))
-                init = tf.uniform_unit_scaling_initializer(1.43)
+                init = tf.truncated_normal_initializer(
+                    stddev=1.0 / math.sqrt(float(self.layer_dim[i - 1])))
+                # init = tf.uniform_unit_scaling_initializer(1.43)
                 layer = tf.layers.dense(pre_layer, self.layer_dim[i], name="layer%d" % i,
                                         activation=self.leaky_relu,
                                         kernel_initializer=init)
@@ -42,9 +42,9 @@ class DNN(NET):
                 # tf.summary.histogram("biases", biases)
 
         with tf.variable_scope("concat"):
-            # init = tf.truncated_normal_initializer(
-            #     stddev=1.0 / math.sqrt(float(self.layer_dim[-1])))
-            init = tf.uniform_unit_scaling_initializer(1.)
+            init = tf.truncated_normal_initializer(
+                stddev=1.0 / math.sqrt(float(self.layer_dim[-1])))
+            # init = tf.uniform_unit_scaling_initializer(1.)
             logits = tf.layers.dense(pre_layer, 1, name="logists",
                                      kernel_initializer=init)
 
