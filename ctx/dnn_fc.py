9b0866080aae9a4c95ee2a89cb2fb7c8331e6d2b
xuwenwen <xuwenwen@xiaomi.com>
2017-10-08 23:26:33 +0800

    deep layers

--- ctx/dnn_fc.py
+++ ctx/dnn_fc.py
@@ -12,15 +12,13 @@ class DNNFC(NET):
         'iid': tf.FixedLenFeature(1, tf.int64),
     }
 
-
     def inference(self, fea, drop=0.4):
         self.step = 10
-        self.ema_factor = 0.999        
+        self.ema_factor = 0.999
         w_fea = fea['wide']
         fea = fea['deep']
 
         with tf.variable_scope("ftrl"):
-
             bias = self.cf.getfloat("net", "w_bias")
             weight_file = self.cf.get("net", "w_weight")
             ftrl_weight = self.load_ftrl_weight(weight_file)
@@ -33,8 +31,8 @@ class DNNFC(NET):
             weights = tf.Variable(ftrl_weight, name="ftrl_weight", trainable=False)
             ftrl = tf.nn.embedding_lookup_sparse(weights, w_fea, None, combiner="sum") + bias
 
-        batch_norm_layer = partial(tf.layers.batch_normalization,
-                                   training=self.training, momentum=0.9)
+        bn_layer = partial(tf.layers.batch_normalization,
+                           training=self.training, momentum=0.9)
 
         self.dense_dim = self.cf.getint("net", "dense_dim")
         with tf.variable_scope("embed"):
@@ -53,9 +51,12 @@ class DNNFC(NET):
                 init = tf.truncated_normal_initializer(
                     stddev=1.0 / math.sqrt(float(self.layer_dim[i - 1])))
                 layer = tf.layers.dense(pre_layer, self.layer_dim[i], name="layer%d" % i,
-                                        activation=self.leaky_relu,
+                                        # activation=self.leaky_relu,
                                         kernel_initializer=init)
+                layer = bn_layer(layer)
+                layer = self.leaky_relu(layer)
                 layer = tf.layers.dropout(layer, drop)
+
                 pre_layer = layer
 
         with tf.variable_scope("concat"):
@@ -63,6 +64,7 @@ class DNNFC(NET):
                 stddev=1.0 / math.sqrt(float(self.layer_dim[-1])))
             logits = tf.layers.dense(pre_layer, 1, name="logists",
                                      kernel_initializer=init)
+            logits = bn_layer(logits)
             logits += ftrl
 
         return logits
