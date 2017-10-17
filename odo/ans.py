a38b9a915871a0da9adf6640e23da4eb07997a0f
xuwenwen <xuwenwen@xiaomi.com>
2017-10-17 23:55:53 +0800

    deep layers

--- odo/ans.py
+++ odo/ans.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         assert len(items) == 3
         # if float(items[1]) <= 0.01:
         # items[1] = "0.01"
-        ans += [[int(items[2]), items[1]]]
+        ans += [[items[2], items[1]]]
 
     ans = sorted(ans, key=lambda x: x[0])
     for key, score in ans:
