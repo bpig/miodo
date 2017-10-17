4527fa9a0abaf741ec0fd970d436ff970f633549
xuwenwen <xuwenwen@xiaomi.com>
2017-10-18 00:55:48 +0800

    deep layers

--- odo/imdb.py
+++ odo/imdb.py
@@ -50,7 +50,7 @@ def infer(fea, training=True):
     with tf.variable_scope("lstm"):
         # cell = tf.contrib.rnn.LSTMCell(num_units=8, use_peepholes=True)
         layers = [tf.contrib.rnn.BasicLSTMCell(num_units=12,
-                                               activation=tf.nn.relu)
+                                               activation=leaky_relu)
                   for layer in range(4)]
         # cell = tf.contrib.rnn.BasicLSTMCell(num_units=8)
         if training:
