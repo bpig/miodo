0a4c6f83353be8fdec8f9693481bc56c4ebf0c92
xuwenwen <xuwenwen@xiaomi.com>
2017-10-18 00:24:31 +0800

    deep layers

--- odo/imdb.py
+++ odo/imdb.py
@@ -133,9 +133,9 @@ def train():
     global_step = tf.train.create_global_step()
     adam = tf.train.AdamOptimizer(learning_rate=0.001)
     grads = adam.compute_gradients(loss)
-    # for i, (g, v) in enumerate(grads):
-    #     if g is not None:
-    #         grads[i] = (tf.clip_by_norm(g, 50), v)
+    for i, (g, v) in enumerate(grads):
+        if g is not None:
+            grads[i] = (tf.clip_by_norm(g, 5), v)
     training_op = adam.apply_gradients(grads, global_step=global_step)
 
     tf.get_variable_scope().reuse_variables()
