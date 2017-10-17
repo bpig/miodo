673b567362b8c98b7eb83637d8d6c36131349358
xuwenwen <xuwenwen@xiaomi.com>
2017-10-18 06:13:48 +0800

    deep layers

--- odo/imdb.py
+++ odo/imdb.py
@@ -58,7 +58,7 @@ def infer(fea, training=True):
     keep_prob = 0.5
     with tf.variable_scope("lstm1"):
         # cell = tf.contrib.rnn.LSTMCell(num_units=8, use_peepholes=True)
-        layers = [tf.contrib.rnn.BasicLSTMCell(num_units=16,
+        layers = [tf.contrib.rnn.BasicLSTMCell(num_units=12,
                                                activation=tf.nn.relu)
                   for _ in range(2)]
         # cell = tf.contrib.rnn.BasicLSTMCell(num_units=8)
@@ -70,7 +70,7 @@ def infer(fea, training=True):
         # states = tf.concat(axis=1, values=states)
 
     with tf.variable_scope("lstm2"):
-        layers = [tf.contrib.rnn.BasicLSTMCell(num_units=16,
+        layers = [tf.contrib.rnn.BasicLSTMCell(num_units=12,
                                                activation=tf.nn.relu)
                   for _ in range(2)]
         if training:
